9723eaa510f6227080cd8c0d007287e2c8eba18a
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-27 11:34:34 +0300

    Add IPreferenceService interface

---
+++ server/FlameDating/FlameDating.Core/Contracts/IPreferenceService.cs
@@ -0,0 +1,9 @@
+﻿using FlameDating.Infrastructure.Enums;
+
+namespace FlameDating.Core.Contracts
+{
+    public interface IPreferenceService
+    {
+        Task<Guid> CreatePreferenceAsync(int maximumDistance, Gender PreferedGender);
+    }
+}
