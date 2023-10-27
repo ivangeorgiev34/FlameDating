bbd6f5af0595cc7c97c8f0980965fa1f23038770
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-27 13:05:05 +0300

    Add ApplicationConstants class for constants

---
+++ server/FlameDating/FlameDating/Constants/ApplicationConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace FlameDating.Constants
+{
+    public static class ApplicationConstants
+    {
+        public static class Response
+        {
+            public const string RESPONSE_STATUS_ERROR = "Error";
+            public const string RESPONSE_STATUS_SUCCESS = "Success";
+        }
+    }
+}
