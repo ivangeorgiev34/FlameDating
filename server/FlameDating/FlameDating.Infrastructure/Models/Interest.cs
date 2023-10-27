e8eaf2d12b4f7ff46159a382a0faa6697eafa143
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-27 09:47:41 +0300

    Add Interest database model

---
+++ server/FlameDating/FlameDating.Infrastructure/Models/Interest.cs
@@ -0,0 +1,14 @@
+﻿using FlameDating.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace FlameDating.Infrastructure.Models
+{
+    public class Interest
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = InfrastructureConstants.Interest.NAME_REQUIRED_ERROR_MESSAGE)]
+        public string Name { get; set; } = null!;
+    }
+}
