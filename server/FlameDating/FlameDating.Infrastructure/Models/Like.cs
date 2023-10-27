ee013f99294719d7ed27a5b9cae027ab82b7918d
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-27 09:47:49 +0300

    Add Like database model

---
+++ server/FlameDating/FlameDating.Infrastructure/Models/Like.cs
@@ -0,0 +1,31 @@
+﻿using FlameDating.Infrastructure.Constants;
+using FlameDating.Infrastructure.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlameDating.Infrastructure.Models
+{
+    public class Like
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = InfrastructureConstants.Like.LIKE_STATUS_REQUIRED_ERROR_MESSAGE)]
+        [EnumDataType(typeof(Gender),
+            ErrorMessage = InfrastructureConstants.Like.LIKE_STATUS_DATA_TYPE_ERROR_MESSAGE)]
+        public LikeStatus LikeStatus { get; set; }
+
+        [Required(ErrorMessage = InfrastructureConstants.Like.LIKE_DATE_REQUIRED_ERROR_MESSAGE)]
+        public DateTime LikeDate { get; set; }
+
+        [Required(ErrorMessage = InfrastructureConstants.Like.LIKER_ID_REQUIRED_ERROR_MESSAGE)]
+        [ForeignKey(nameof(Liker))]
+        public Guid LikerUserId { get; set; }
+        public User Liker { get; set; } = null!;
+
+        [Required(ErrorMessage = InfrastructureConstants.Like.LIKED_ID_REQUIRED_ERROR_MESSAGE)]
+        [ForeignKey(nameof(Liked))]
+        public Guid LikedUserId { get; set; }
+        public User Liked { get; set; } = null!;
+    }
+}
