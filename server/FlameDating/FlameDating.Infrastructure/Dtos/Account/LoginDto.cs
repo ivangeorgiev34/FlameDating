6cc9270dfa28246e8559df7a245ee8abcd24259d
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-27 11:30:14 +0300

    Add LoginDto DTO

---
+++ server/FlameDating/FlameDating.Infrastructure/Dtos/Account/LoginDto.cs
@@ -0,0 +1,15 @@
+﻿using FlameDating.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace FlameDating.Infrastructure.Dtos.Account
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = InfrastructureConstants.RegisterDto.EMAIL_REQUIRED_ERROR_MESSAGE)]
+        [EmailAddress(ErrorMessage = InfrastructureConstants.RegisterDto.EMAIL_INVALID_ERROR_MESSAGE)]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = InfrastructureConstants.RegisterDto.PASSWORD_REQUIRED_ERROR_MESSAGE)]
+        public string Password { get; set; } = null!;
+    }
+}
