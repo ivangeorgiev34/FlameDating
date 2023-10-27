900d037601af7238f92882390dedd3ba71c8b8a5
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-27 11:34:16 +0300

    Add ConvertProfilePictureIntoByteArray method implementation

---
+++ server/FlameDating/FlameDating.Core/Services/AccountService.cs
@@ -0,0 +1,24 @@
+﻿using FlameDating.Core.Contracts;
+using Microsoft.AspNetCore.Http;
+
+namespace FlameDating.Core.Services
+{
+    public class AccountService : IAccountService
+    {
+        public async Task<byte[]?> ConvertProfilePictureIntoByteArray(IFormFile? profilePicture)
+        {
+            byte[]? imageBytes = null;
+
+            if (profilePicture != null)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    await profilePicture.CopyToAsync(ms);
+                    imageBytes = ms.ToArray();
+                }
+            }
+
+            return imageBytes;
+        }
+    }
+}
