4a1e384ef2f44a7ead1c91accc7c2bb3e302b48a
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-27 12:39:32 +0300

    Add EditProfileAsync method

--- server/FlameDating/FlameDating.Core/Contracts/IAccountService.cs
+++ server/FlameDating/FlameDating.Core/Contracts/IAccountService.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FlameDating.Infrastructure.Dtos.Account;
+using FlameDating.Infrastructure.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace FlameDating.Core.Contracts
 {
     public interface IAccountService
     {
         Task<byte[]?> ConvertProfilePictureIntoByteArray(IFormFile? profilePicture);
+
+        Task EditProfileAsync(User user, EditProfileDto editProfileDto);
     }
 }
