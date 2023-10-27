42bc8a21cd640041c79c1601866714913ba67173
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-27 09:47:57 +0300

    Add Match database model

---
+++ server/FlameDating/FlameDating.Infrastructure/Models/Match.cs
@@ -0,0 +1,30 @@
+﻿using FlameDating.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlameDating.Infrastructure.Models
+{
+    public class Match
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = InfrastructureConstants.Match.MATCH_DATE_REQUIRED_ERROR_MESSAGE)]
+        public DateTime MatchDate { get; set; }
+
+        [Required(ErrorMessage = InfrastructureConstants.Match.FIRST_USER_ID_REQUIRED_ERROR_MESSAGE)]
+        [ForeignKey(nameof(FirstUser))]
+        public Guid FirstUserId { get; set; }
+        public User FirstUser { get; set; } = null!;
+
+        [Required(ErrorMessage = InfrastructureConstants.Match.SECOND_USER_ID_REQUIRED_ERROR_MESSAGE)]
+        [ForeignKey(nameof(SecondUser))]
+        public Guid SecondUserId { get; set; }
+        public User SecondUser { get; set; } = null!;
+
+        [Required(ErrorMessage = InfrastructureConstants.Match.SECOND_USER_ID_REQUIRED_ERROR_MESSAGE)]
+        [ForeignKey(nameof(Chat))]
+        public Guid ChatId { get; set; }
+        public Chat Chat { get; set; } = null!;
+    }
+}
