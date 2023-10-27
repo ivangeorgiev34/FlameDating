b81e65d8b4e34706e233a7e14de6b57cc990fc79
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-27 10:36:02 +0300

    Add ServiceCollectionExtension class for adding services

---
+++ server/FlameDating/FlameDating/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,47 @@
+﻿using FlameDating.Core.Contracts;
+using FlameDating.Core.Services;
+using FlameDating.Infrastructure.Common;
+using FlameDating.Infrastructure.Data;
+using FlameDating.Infrastructure.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace FlameDating.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddIdentity<User, IdentityRole<Guid>>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredUniqueChars = 0;
+            })
+                .AddEntityFrameworkStores<FlameDatingDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddCors(options =>
+            {
+                //options.AddPolicy("LocalServer", policy =>
+                //{
+                //    policy.WithOrigins("http://127.0.0.1:5500")
+                //    .AllowAnyMethod()
+                //    .AllowAnyHeader();
+                //});
+
+                options.AddPolicy("LocalServer", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+
+            services.AddScoped<IRepository, Repository>();
+            services.AddScoped<IPreferenceService, PreferenceService>();
+        }
+    }
+}
