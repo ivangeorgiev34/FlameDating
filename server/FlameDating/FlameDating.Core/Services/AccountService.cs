e6512d90574680a82e490443da3223c05ed85e1e
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-27 12:39:42 +0300

    Add EditProfileAsync method implementation

--- server/FlameDating/FlameDating.Core/Services/AccountService.cs
+++ server/FlameDating/FlameDating.Core/Services/AccountService.cs
@@ -1,10 +1,23 @@
 ﻿using FlameDating.Core.Contracts;
+using FlameDating.Infrastructure.Common;
+using FlameDating.Infrastructure.Dtos.Account;
+using FlameDating.Infrastructure.Models;
 using Microsoft.AspNetCore.Http;
 
 namespace FlameDating.Core.Services
 {
     public class AccountService : IAccountService
     {
+        private readonly IRepository repo;
+        private readonly IPreferenceService preferenceService;
+
+        public AccountService(IRepository _repo,
+            IPreferenceService _preferenceService)
+        {
+            this.repo = _repo;
+            this.preferenceService = _preferenceService;
+        }
+
         public async Task<byte[]?> ConvertProfilePictureIntoByteArray(IFormFile? profilePicture)
         {
             byte[]? imageBytes = null;
@@ -19,6 +32,36 @@ namespace FlameDating.Core.Services
             }
 
             return imageBytes;
+        }
+
+        public async Task EditProfileAsync(User user, EditProfileDto editProfileDto)
+        {
+            var userPreference = await preferenceService.GetPreferenceByIdAsync(user.PreferenceId);
+
+            if (userPreference == null)
+            {
+                throw new NullReferenceException("User doesn't have preference");
+            }
+
+            userPreference.Gender = editProfileDto.Gender;
+            userPreference.MaximumDistance = editProfileDto.MaximumDistance;
+
+            user.FirstName = editProfileDto.FirstName;
+            user.MiddleName = editProfileDto.MiddleName;
+            user.LastName = editProfileDto.LastName;
+            user.Age = editProfileDto.Age;
+            user.Gender = editProfileDto.Gender;
+            user.Height = editProfileDto.Height;
+            user.School = editProfileDto.School;
+            user.Job = editProfileDto.Job;
+            user.Biography = editProfileDto.Biography;
+            user.FirstProfilePicture = (await ConvertProfilePictureIntoByteArray(editProfileDto.FirstProfilePicture))!;
+            user.SecondProfilePicture = await ConvertProfilePictureIntoByteArray(editProfileDto?.SecondProfilePicture);
+            user.ThirdProfilePicture = await ConvertProfilePictureIntoByteArray(editProfileDto?.ThirdProfilePicture);
+            user.FourthProfilePicture = await ConvertProfilePictureIntoByteArray(editProfileDto?.FourthProfilePicture);
+            user.FifthProfilePicture = await ConvertProfilePictureIntoByteArray(editProfileDto?.FifthProfilePicture);
+
+            await repo.SaveChangesAsync();
         }
     }
 }
