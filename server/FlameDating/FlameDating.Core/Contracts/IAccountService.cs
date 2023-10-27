cc6fb957f786a8ef542b6fa45e37471abe688f20
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-27 11:31:34 +0300

    Add

---
+++ server/FlameDating/FlameDating.Core/Contracts/IAccountService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FlameDating.Core.Contracts
+{
+    public interface IAccountService
+    {
+        Task<byte[]?> ConvertProfilePictureIntoByteArray(IFormFile? profilePicture);
+    }
+}
