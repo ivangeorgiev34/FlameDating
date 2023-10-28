ce43d6c54219dc92578a1a1b0b07b430d137455c
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-29 00:27:12 +0300

    Add CreateMatchAsync method implementation

---
+++ server/FlameDating/FlameDating.Core/Services/MatchService.cs
@@ -0,0 +1,35 @@
+﻿using FlameDating.Core.Contracts;
+using FlameDating.Infrastructure.Common;
+using FlameDating.Infrastructure.Models;
+
+namespace FlameDating.Core.Services
+{
+    public class MatchService : IMatchService
+    {
+        private readonly IRepository repo;
+        private readonly IChatService chatService;
+
+        public MatchService(IRepository _repo,
+            IChatService _chatService)
+        {
+            this.repo = _repo;
+            this.chatService = _chatService;
+        }
+
+        public async Task CreateMatchAsync(Guid firstUserId, Guid secondUserId)
+        {
+            var chatId = await chatService.CreateChatAsync();
+
+            var match = new Match()
+            {
+                FirstUserId = firstUserId,
+                SecondUserId = secondUserId,
+                ChatId = chatId,
+                MatchDate = DateTime.Now
+            };
+
+            await repo.AddAsync(match);
+            await repo.SaveChangesAsync();
+        }
+    }
+}
