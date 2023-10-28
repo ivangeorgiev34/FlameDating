43912fc82894f3caf5c9bee0243aa713fdb49bb3
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-29 00:30:16 +0300

    Add BaseController

---
+++ server/FlameDating/FlameDating/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FlameDating.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected string? GetUserId(HttpContext httpContext)
+        {
+            var claimsIdentity = (ClaimsIdentity?)httpContext.User.Identity ?? null;
+            var userIdClaim = claimsIdentity?.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+
+            return userIdClaim?.Value;
+        }
+
+        protected bool IsIdValidGuidAndNotNull(string id)
+        {
+            if (id == null || Guid.TryParse(id, out Guid result) == false)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
