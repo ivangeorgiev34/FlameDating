d3498b2b1ae8eb63b30fc497f6170aa211b0bbe7
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-29 00:26:17 +0300

    Add LikeUserAsync method implementation

---
+++ server/FlameDating/FlameDating.Core/Services/LikeService.cs
@@ -0,0 +1,50 @@
+﻿using FlameDating.Core.Contracts;
+using FlameDating.Infrastructure.Common;
+using FlameDating.Infrastructure.Enums;
+using FlameDating.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlameDating.Core.Services
+{
+    public class LikeService : ILikeService
+    {
+        private readonly IRepository repo;
+        private readonly IMatchService matchService;
+
+        public LikeService(IRepository _repo,
+            IMatchService _matchService)
+        {
+            this.repo = _repo;
+            this.matchService = _matchService;
+        }
+
+        public async Task<bool> LikeUserAsync(Guid likerUserId, Guid likedUserId)
+        {
+            var isLikerLiked = await repo.AllReadonly<Like>()
+                .AnyAsync(l => l.LikedUserId == likerUserId
+                    && l.LikerUserId == likedUserId);
+
+            if (isLikerLiked == true)
+            {
+                await matchService.CreateMatchAsync(likerUserId, likedUserId);
+
+                return true;
+            }
+
+            var like = new Like()
+            {
+                LikerUserId = likerUserId,
+                LikedUserId = likedUserId,
+                LikeStatus = LikeStatus.Like,
+                LikeDate = DateTime.Now
+            };
+
+            await repo.AddAsync(like);
+            await repo.SaveChangesAsync();
+
+            return true;
+
+
+        }
+    }
+}
