793a9eed57c60c84c474154ecf3717b77af30eb4
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-10-29 00:25:40 +0300

    Add CreateChatAsync method implementation

---
+++ server/FlameDating/FlameDating.Core/Services/ChatService.cs
@@ -0,0 +1,25 @@
+﻿using FlameDating.Core.Contracts;
+using FlameDating.Infrastructure.Common;
+using FlameDating.Infrastructure.Models;
+
+namespace FlameDating.Core.Services
+{
+    public class ChatService : IChatService
+    {
+        private readonly IRepository repo;
+
+        public ChatService(IRepository _repo)
+        {
+            this.repo = _repo;
+        }
+        public async Task<Guid> CreateChatAsync()
+        {
+            var chat = new Chat();
+
+            await repo.AddAsync(chat);
+            await repo.SaveChangesAsync();
+
+            return chat.Id;
+        }
+    }
+}
