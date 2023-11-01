0de6e75f0a627e151c8341ac105b53945f2f393f
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-01 16:57:07 +0200

    Add GetUsersChats action

---
+++ server/FlameDating/FlameDating/Controllers/ChatController.cs
@@ -0,0 +1,41 @@
+﻿using FlameDating.Constants;
+using FlameDating.Core.Contracts;
+using FlameDating.Utilities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlameDating.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : BaseController
+    {
+        private readonly IChatService chatService;
+
+        public ChatController(IChatService _chatService)
+        {
+            this.chatService = _chatService;
+        }
+
+        [HttpGet]
+        [Route("/chats")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> GetUsersChats()
+        {
+            var userId = GetUserId(HttpContext);
+
+            var chats = await chatService.GetUsersChatsAsync(Guid.Parse(userId!));
+
+            return StatusCode(StatusCodes.Status200OK, new Response
+            {
+                Status = ApplicationConstants.Response.RESPONSE_STATUS_SUCCESS,
+                Message = "Successfully retrieved chats",
+                Content = new
+                {
+                    chats = chats
+                }
+            });
+        }
+    }
+}
