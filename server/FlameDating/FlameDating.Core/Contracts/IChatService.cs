94e1fba82be4efd206e8432d9ec2eb9e6be46221
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-01 16:57:32 +0200

    Add GetUsersChats method

--- server/FlameDating/FlameDating.Core/Contracts/IChatService.cs
+++ server/FlameDating/FlameDating.Core/Contracts/IChatService.cs
@@ -1,7 +1,11 @@
-﻿namespace FlameDating.Core.Contracts
+﻿using FlameDating.Infrastructure.Dtos.Chat;
+
+namespace FlameDating.Core.Contracts
 {
     public interface IChatService
     {
         Task<Guid> CreateChatAsync();
+
+        Task<List<GetUsersChatsDto>> GetUsersChatsAsync(Guid userId);
     }
 }
