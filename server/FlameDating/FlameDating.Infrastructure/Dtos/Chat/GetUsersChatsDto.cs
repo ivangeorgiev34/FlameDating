77815253984cea5ae19e23e8891140f2263e6b2f
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-01 16:58:38 +0200

    Add GetUsersChatsDto for data transfer

---
+++ server/FlameDating/FlameDating.Infrastructure/Dtos/Chat/GetUsersChatsDto.cs
@@ -0,0 +1,13 @@
+﻿using FlameDating.Infrastructure.Dtos.Account;
+
+namespace FlameDating.Infrastructure.Dtos.Chat
+{
+    public class GetUsersChatsDto
+    {
+        public string Id { get; set; } = null!;
+
+        public UserDto CurrentUser { get; set; } = null!;
+
+        public UserDto RecipientUser { get; set; } = null!;
+    }
+}
