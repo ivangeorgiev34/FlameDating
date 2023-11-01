f64cc181f0a5ba895241ecfada9ac186d6a0447f
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-01 16:57:48 +0200

    Add GetUsersChats method implementation

--- server/FlameDating/FlameDating.Core/Services/ChatService.cs
+++ server/FlameDating/FlameDating.Core/Services/ChatService.cs
@@ -1,6 +1,9 @@
 ﻿using FlameDating.Core.Contracts;
 using FlameDating.Infrastructure.Common;
+using FlameDating.Infrastructure.Dtos.Account;
+using FlameDating.Infrastructure.Dtos.Chat;
 using FlameDating.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlameDating.Core.Services
 {
@@ -20,6 +23,137 @@ namespace FlameDating.Core.Services
             await repo.SaveChangesAsync();
 
             return chat.Id;
+        }
+
+        public async Task<List<GetUsersChatsDto>> GetUsersChatsAsync(Guid userId)
+        {
+            var chats = await repo.AllReadonly<Match>()
+                .Include(m => m.FirstUser)
+                .Include(m => m.SecondUser)
+                .Include(m => m.Chat)
+                .OrderBy(m => m.Chat.Messages.Count(m => m.IsSeen == false))
+                .Where(m => m.FirstUserId == userId || m.SecondUserId == userId)
+                .Select(m => new GetUsersChatsDto()
+                {
+                    Id = m.Id.ToString(),
+                    CurrentUser = m.FirstUserId == userId
+                    ? new UserDto()
+                    {
+                        Id = m.FirstUserId.ToString(),
+                        Age = m.FirstUser.Age,
+                        Biography = m.FirstUser.Biography,
+                        FirstName = m.FirstUser.FirstName,
+                        LastName = m.FirstUser.LastName,
+                        MiddleName = m.FirstUser.MiddleName,
+                        Gender = m.FirstUser.Gender,
+                        Job = m.FirstUser.Job,
+                        Height = m.FirstUser.Height,
+                        LocationLatitude = m.FirstUser.LocationLatitude,
+                        LocationLongitude = m.FirstUser.LocationLongitude,
+                        School = m.FirstUser.School,
+                        FirstProfilePicture = Convert.ToBase64String(m.FirstUser.FirstProfilePicture),
+                        SecondProfilePicture = m.FirstUser.SecondProfilePicture != null
+                        ? Convert.ToBase64String(m.FirstUser.SecondProfilePicture)
+                        : null,
+                        ThirdProfilePicture = m.FirstUser.ThirdProfilePicture != null
+                        ? Convert.ToBase64String(m.FirstUser.ThirdProfilePicture)
+                        : null,
+                        FourthProfilePicture = m.FirstUser.FourthProfilePicture != null
+                        ? Convert.ToBase64String(m.FirstUser.FourthProfilePicture)
+                        : null,
+                        FifthProfilePicture = m.FirstUser.FifthProfilePicture != null
+                        ? Convert.ToBase64String(m.FirstUser.FifthProfilePicture)
+                        : null
+                    }
+                    : new UserDto()
+                    {
+                        Id = m.SecondUserId.ToString(),
+                        Age = m.SecondUser.Age,
+                        Biography = m.SecondUser.Biography,
+                        FirstName = m.SecondUser.FirstName,
+                        LastName = m.SecondUser.LastName,
+                        MiddleName = m.SecondUser.MiddleName,
+                        Gender = m.SecondUser.Gender,
+                        Job = m.SecondUser.Job,
+                        Height = m.SecondUser.Height,
+                        LocationLatitude = m.SecondUser.LocationLatitude,
+                        LocationLongitude = m.SecondUser.LocationLongitude,
+                        School = m.SecondUser.School,
+                        FirstProfilePicture = Convert.ToBase64String(m.SecondUser.FirstProfilePicture),
+                        SecondProfilePicture = m.SecondUser.SecondProfilePicture != null
+                        ? Convert.ToBase64String(m.SecondUser.SecondProfilePicture)
+                        : null,
+                        ThirdProfilePicture = m.SecondUser.ThirdProfilePicture != null
+                        ? Convert.ToBase64String(m.SecondUser.ThirdProfilePicture)
+                        : null,
+                        FourthProfilePicture = m.SecondUser.FourthProfilePicture != null
+                        ? Convert.ToBase64String(m.SecondUser.FourthProfilePicture)
+                        : null,
+                        FifthProfilePicture = m.SecondUser.FifthProfilePicture != null
+                        ? Convert.ToBase64String(m.SecondUser.FifthProfilePicture)
+                        : null
+                    },
+                    RecipientUser = m.SecondUserId == userId
+                    ? new UserDto()
+                    {
+                        Id = m.FirstUserId.ToString(),
+                        Age = m.FirstUser.Age,
+                        Biography = m.FirstUser.Biography,
+                        FirstName = m.FirstUser.FirstName,
+                        LastName = m.FirstUser.LastName,
+                        MiddleName = m.FirstUser.MiddleName,
+                        Gender = m.FirstUser.Gender,
+                        Job = m.FirstUser.Job,
+                        Height = m.FirstUser.Height,
+                        LocationLatitude = m.FirstUser.LocationLatitude,
+                        LocationLongitude = m.FirstUser.LocationLongitude,
+                        School = m.FirstUser.School,
+                        FirstProfilePicture = Convert.ToBase64String(m.FirstUser.FirstProfilePicture),
+                        SecondProfilePicture = m.FirstUser.SecondProfilePicture != null
+                        ? Convert.ToBase64String(m.FirstUser.SecondProfilePicture)
+                        : null,
+                        ThirdProfilePicture = m.FirstUser.ThirdProfilePicture != null
+                        ? Convert.ToBase64String(m.FirstUser.ThirdProfilePicture)
+                        : null,
+                        FourthProfilePicture = m.FirstUser.FourthProfilePicture != null
+                        ? Convert.ToBase64String(m.FirstUser.FourthProfilePicture)
+                        : null,
+                        FifthProfilePicture = m.FirstUser.FifthProfilePicture != null
+                        ? Convert.ToBase64String(m.FirstUser.FifthProfilePicture)
+                        : null
+                    }
+                    : new UserDto()
+                    {
+                        Id = m.SecondUserId.ToString(),
+                        Age = m.SecondUser.Age,
+                        Biography = m.SecondUser.Biography,
+                        FirstName = m.SecondUser.FirstName,
+                        LastName = m.SecondUser.LastName,
+                        MiddleName = m.SecondUser.MiddleName,
+                        Gender = m.SecondUser.Gender,
+                        Job = m.SecondUser.Job,
+                        Height = m.SecondUser.Height,
+                        LocationLatitude = m.SecondUser.LocationLatitude,
+                        LocationLongitude = m.SecondUser.LocationLongitude,
+                        School = m.SecondUser.School,
+                        FirstProfilePicture = Convert.ToBase64String(m.SecondUser.FirstProfilePicture),
+                        SecondProfilePicture = m.SecondUser.SecondProfilePicture != null
+                        ? Convert.ToBase64String(m.SecondUser.SecondProfilePicture)
+                        : null,
+                        ThirdProfilePicture = m.SecondUser.ThirdProfilePicture != null
+                        ? Convert.ToBase64String(m.SecondUser.ThirdProfilePicture)
+                        : null,
+                        FourthProfilePicture = m.SecondUser.FourthProfilePicture != null
+                        ? Convert.ToBase64String(m.SecondUser.FourthProfilePicture)
+                        : null,
+                        FifthProfilePicture = m.SecondUser.FifthProfilePicture != null
+                        ? Convert.ToBase64String(m.SecondUser.FifthProfilePicture)
+                        : null
+                    }
+                })
+                .ToListAsync();
+
+            return chats;
         }
     }
 }
