08aaaea5de47b048ffa93ed9302b10a02d9e511c
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-02 22:29:46 +0200

    Add AddMessageAsync method implementation

---
+++ server/FlameDating/FlameDating.Core/Services/MessageService.cs
@@ -0,0 +1,41 @@
+﻿using FlameDating.Core.Contracts;
+using FlameDating.Infrastructure.Common;
+using FlameDating.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlameDating.Core.Services
+{
+    public class MessageService : IMessageService
+    {
+        private readonly IRepository repo;
+
+        public MessageService(IRepository _repo)
+        {
+            this.repo = _repo;
+        }
+
+        public async Task AddMessageAsync(string text, Guid chatId, Guid senderId, Guid recieverId)
+        {
+            var chat = await repo.All<Chat>()
+                .Include(c => c.Messages)
+                .FirstOrDefaultAsync(c => c.Id == chatId);
+
+            if (chat == null)
+            {
+                throw new NullReferenceException("Chat does not exist");
+            }
+
+            var message = new Message()
+            {
+                Text = text,
+                Date = DateTime.Now,
+                RecieverUserId = recieverId,
+                SenderUserId = senderId
+            };
+
+            chat.Messages.Add(message);
+
+            await repo.SaveChangesAsync();
+        }
+    }
+}
