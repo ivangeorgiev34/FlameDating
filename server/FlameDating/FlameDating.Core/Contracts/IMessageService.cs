d441dffab880005b4519fcc5e651cdc4253cfe25
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-02 22:29:35 +0200

    Add AddMessageAsync method

---
+++ server/FlameDating/FlameDating.Core/Contracts/IMessageService.cs
@@ -0,0 +1,7 @@
+﻿namespace FlameDating.Core.Contracts
+{
+    public interface IMessageService
+    {
+        Task AddMessageAsync(string text, Guid chatId, Guid senderId, Guid recieverId);
+    }
+}
