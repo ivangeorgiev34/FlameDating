55b447b8aee3cd0945c230ad6ebfc7ef204f879c
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-03 23:03:48 +0200

    Add SendMessageAsync in ChatHub hub

---
+++ server/FlameDating/FlameDating/Hubs/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using FlameDating.Core.Contracts;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Net.WebSockets;
+
+namespace FlameDating.Hubs
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class ChatHub : Hub
+    {
+        private readonly IMessageService messageService;
+
+        public ChatHub(IMessageService _messageService)
+        {
+            this.messageService = _messageService;
+        }
+
+        public async Task SendMessageAsync(string text, string chatId, string recieverId)
+        {
+            var senderId = this.Context.UserIdentifier;
+
+            await messageService.AddMessageAsync(text, Guid.Parse(chatId), Guid.Parse(senderId),
+                Guid.Parse(recieverId));
+
+            await this.Clients.User(recieverId.ToLower())
+                .SendAsync("RecieveMessage", senderId, text);
+        }
+    }
+}
+
