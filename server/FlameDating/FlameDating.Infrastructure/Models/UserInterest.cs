ed0dbcce1000a6ac69713d2cb542132d88d55841
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-07 20:06:49 +0200

    Add UserInterest database entity model

---
+++ server/FlameDating/FlameDating.Infrastructure/Models/UserInterest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlameDating.Infrastructure.Models
+{
+    public class UserInterest
+    {
+        [ForeignKey(nameof(User))]
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        [ForeignKey(nameof(Interest))]
+        public Guid InterestId { get; set; }
+        public Interest Interest { get; set; } = null!;
+    }
+}
