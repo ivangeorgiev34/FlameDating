98e685f859f0c80d32244803c218d7c5c30bd585
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-07 20:23:35 +0200

    Add CreateInterestsDto DTO

---
+++ server/FlameDating/FlameDating.Infrastructure/Dtos/Interest/CreateInterestsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FlameDating.Infrastructure.Dtos.Interest
+{
+    public class CreateInterestsDto
+    {
+        public CreateInterestsDto()
+        {
+            this.InterestsIds = new List<string>();
+        }
+        public List<string> InterestsIds { get; set; } = null!;
+    }
+}
