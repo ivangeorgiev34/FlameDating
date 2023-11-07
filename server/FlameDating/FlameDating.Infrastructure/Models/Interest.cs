2eb17c4cb7f15b7489f1668876be034a4f729e05
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-07 20:19:30 +0200

    Add entity relation to UserInterest

--- server/FlameDating/FlameDating.Infrastructure/Models/Interest.cs
+++ server/FlameDating/FlameDating.Infrastructure/Models/Interest.cs
@@ -5,10 +5,17 @@ namespace FlameDating.Infrastructure.Models
 {
     public class Interest
     {
+        public Interest()
+        {
+            this.UsersInterests = new List<UserInterest>();
+        }
+
         [Key]
         public Guid Id { get; set; }
 
         [Required(ErrorMessage = InfrastructureConstants.Interest.NAME_REQUIRED_ERROR_MESSAGE)]
         public string Name { get; set; } = null!;
+
+        public ICollection<UserInterest> UsersInterests { get; set; } = null!;
     }
 }
