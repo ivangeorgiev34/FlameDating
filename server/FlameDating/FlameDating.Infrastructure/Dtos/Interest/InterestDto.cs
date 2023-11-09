dbf736d1f7bcda4f96538c21e3e3be3f4a07e931
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-09 10:39:49 +0200

    Add InterestDto DTO

---
+++ server/FlameDating/FlameDating.Infrastructure/Dtos/Interest/InterestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FlameDating.Infrastructure.Dtos.Interest
+{
+    public class InterestDto
+    {
+        public string Id { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+    }
+}
