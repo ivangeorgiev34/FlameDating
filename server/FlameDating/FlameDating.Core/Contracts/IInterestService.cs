584c5e786d390c34377ea0f04cd0b66393462fba
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-09 10:40:31 +0200

    Add GetAllInterestsAsync method

--- server/FlameDating/FlameDating.Core/Contracts/IInterestService.cs
+++ server/FlameDating/FlameDating.Core/Contracts/IInterestService.cs
@@ -1,7 +1,11 @@
-﻿namespace FlameDating.Core.Contracts
+﻿using FlameDating.Infrastructure.Dtos.Interest;
+
+namespace FlameDating.Core.Contracts
 {
     public interface IInterestService
     {
         Task CreateInterestsByIdsAsync(Guid userId, List<string> interestsIds);
+
+        Task<IEnumerable<InterestDto>> GetAllInterestsAsync();
     }
 }
