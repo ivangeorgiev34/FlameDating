0a8f3e83b7e389576dbebd3001f82cbfd61c838f
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-10 12:22:33 +0200

    Add IAuthState interface

---
+++ src/interfaces/auth/IAuthState.tsx
@@ -0,0 +1,21 @@
+export default interface IAuthState {
+  id: string | null;
+  firstName: string | null;
+  middleName: string | null;
+  lastName: string | null;
+  age: number | null;
+  email: string | null;
+  username: string | null;
+  gender: 0 | 1 | null;
+  biography: string | null;
+  school: string | null;
+  job: string | null;
+  height: number | null;
+  firstProfilePicture: string | null;
+  secondProfilePicture: string | null;
+  thirdProfilePicture: string | null;
+  fourthProfilePicture: string | null;
+  fifthProfilePicture: string | null;
+  token: string | null;
+  expires: string | null;
+}
