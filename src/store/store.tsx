f0b2cbcaa517054a47e60e82ea8f96f972e82d1d
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-16 12:50:13 +0200

    Add store file for configuring the redux store

---
+++ src/store/store.tsx
@@ -0,0 +1,21 @@
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import authReducer from "./auth";
+import loaderReducer from "./loader";
+import menuReducer from "./menu";
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    loader: loaderReducer,
+    menu: menuReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
