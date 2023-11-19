ece6b4d853a81eb6d3189a75c8d835f5ab7fa13f
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-19 20:00:04 +0200

    Add logo to Navigation component

--- src/components/layout/navigation/Navigation.tsx
+++ src/components/layout/navigation/Navigation.tsx
@@ -4,29 +4,43 @@ import { animated } from "@react-spring/web";
 import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHooks";
 import { makeMenuInvisible, makeMenuVisible } from "../../../store/menu";
 import { Menu } from "./menu/Menu";
+import { Link } from "react-router-dom";
 
 export const Navigation: React.FC = () => {
   const dispatch = useAppDispatch();
   const { isVisible } = useAppSelector((state) => state.menu);
+  const { token } = useAppSelector((state) => state.auth);
 
   return (
     <nav className={styles.navBar}>
-      <button
-        className={styles.menuBtn}
-        onClick={() => {
-          isVisible === true
-            ? dispatch(makeMenuInvisible())
-            : dispatch(makeMenuVisible());
-        }}
-      >
-        {isVisible === true ? (
-          <i
-            className={"fa-regular fa-circle-xmark fa-spin fa-flip-horizontal"}
-          ></i>
+      <div className={styles.menuLogoWrapper}>
+        <Link to={"/"} className={styles.logo}>
+          <img src="flame-dating-logo.png" alt="flame-dating-logo" />
+          <span>FlameDating</span>
+        </Link>
+        {token !== null ? (
+          <button
+            className={styles.menuBtn}
+            onClick={() => {
+              isVisible === true
+                ? dispatch(makeMenuInvisible())
+                : dispatch(makeMenuVisible());
+            }}
+          >
+            {isVisible === true ? (
+              <i
+                className={
+                  "fa-regular fa-circle-xmark fa-spin fa-flip-horizontal"
+                }
+              ></i>
+            ) : (
+              <i className={"fa-solid fa-bars"}></i>
+            )}
+          </button>
         ) : (
-          <i className={"fa-solid fa-bars"}></i>
+          <></>
         )}
-      </button>
+      </div>
       <div className={styles.btnsContainer}>
         <a>Login</a>
         <a>Register</a>
