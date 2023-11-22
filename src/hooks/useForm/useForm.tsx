d91cc4d462bb5b80b6ffdf9dc14d129d8b1da4c3
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-22 13:29:33 +0200

    Add useForm hook

---
+++ src/hooks/useForm/useForm.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export function useForm<T>(initialFormValues: T) {
+  let [formValues, setFormValues] = useState(initialFormValues);
+
+  const onFormChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const eventTarget = e.currentTarget as HTMLInputElement;
+
+    setFormValues((state) => ({
+      ...state,
+      [eventTarget.name]: eventTarget.value,
+    }));
+  };
+
+  const onFormChangeImage = (e: React.FormEvent<HTMLInputElement>) => {
+    const eventTarget = e.currentTarget as HTMLInputElement;
+
+    setFormValues((state) => ({
+      ...state,
+      [eventTarget.name]: eventTarget.files?.item(0),
+    }));
+  };
+
+  const setDefaultValues = (values: T) => {
+    setFormValues(values);
+  };
+
+  return {
+    formValues,
+    onFormChange,
+    setDefaultValues,
+    onFormChangeImage,
+  };
+}
