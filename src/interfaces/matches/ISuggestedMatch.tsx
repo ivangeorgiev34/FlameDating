b352acac2c22f30be5bf2ec163b702eb7a5bbb29
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-23 10:58:59 +0200

    Add ISuggestedMatch interface

---
+++ src/interfaces/matches/ISuggestedMatch.tsx
@@ -0,0 +1,19 @@
+export default interface ISuggestedMatch {
+  id: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  age: number;
+  email: string;
+  username: string;
+  gender: 0 | 1;
+  biography: string | null;
+  school: string | null;
+  job: string | null;
+  height: number;
+  firstProfilePicture: string;
+  secondProfilePicture: string | null;
+  thirdProfilePicture: string | null;
+  fourthProfilePicture: string | null;
+  fifthProfilePicture: string | null;
+}
