e1b103ac512459f5749402dbd02281bc01256753
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-23 10:58:43 +0200

    Add private and public route guards

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./App.css";
+import { Loader } from "./components/loader/Loader";
+import { Navigation } from "./components/layout/navigation/Navigation";
+import { Menu } from "./components/layout/navigation/menu/Menu";
+import { useAppSelector } from "./hooks/reduxHooks";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { Login } from "./pages/Login/Login";
+import { PublicRouteGuard } from "./guards/PublicRouteGuard/PublicRouteGuard";
+import { PrivateRouteGuard } from "./guards/PrivateRouteGuard/PrivateRouteGuard";
+import { Matches } from "./pages/Matches/Matches";
 
 function App() {
-  return <React.Fragment></React.Fragment>;
+  const { isVisible } = useAppSelector((state) => state.menu);
+  const { isLoading } = useAppSelector((state) => state.loader);
+
+  return (
+    <React.Fragment>
+      {isLoading === true ? <Loader /> : <></>}
+      <Navigation />
+      <main>
+        {isVisible === true ? <Menu /> : <></>}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route element={<PublicRouteGuard />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+          <Route element={<PrivateRouteGuard />}>
+            <Route path="/matches" element={<Matches />} />
+          </Route>
+        </Routes>
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
