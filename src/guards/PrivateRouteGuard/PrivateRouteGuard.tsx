9fec996ba2c04d1537f3715aba5f74f6e4c89c68
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-11-23 09:31:47 +0200

    Add PrivateRouteGuard component

---
+++ src/guards/PrivateRouteGuard/PrivateRouteGuard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
+import { Navigate, Outlet } from "react-router-dom";
+import { logout } from "../../store/auth";
+
+export const PrivateRouteGuard: React.FC = () => {
+  const { expires } = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch();
+
+  if (expires === null) {
+    return <Navigate to={"/login"} />;
+  } else if (new Date(expires!).getTime() <= Date.now()) {
+    dispatch(logout());
+
+    return <Navigate to={"/login"} />;
+  }
+
+  return <Outlet />;
+};
