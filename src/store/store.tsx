58d7880f0df8ca5916c7c7ea21aecd289b45560f
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-12-04 23:45:40 +0200

    Add interestsReducer in store

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -2,12 +2,14 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import authReducer from "./auth";
 import loaderReducer from "./loader";
 import menuReducer from "./menu";
+import interestsReducer from "./interests";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     loader: loaderReducer,
     menu: menuReducer,
+    interests: interestsReducer,
   },
 });
 
