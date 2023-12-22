7b49f52def4f87d2d969b2b3011719edab09321e
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-12-22 19:49:37 +0200

    Add InternalServerError component content

--- src/pages/InternalServerError/InternalServerError.tsx
+++ src/pages/InternalServerError/InternalServerError.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import styles from "./InternalServerError.module.scss";
+import { Link } from "react-router-dom";
 
 export const InternalServerError: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className={styles.errorWrapper}>
+      <div className={styles.errorCard}>
+        <h2 className={styles.errorTitle}>500 Internal Server Error</h2>
+        <p className={styles.errorDescription}>
+          An unexpected error in the server has occured
+        </p>
+        <Link to={"/"}>Back to home</Link>
+      </div>
+    </div>
+  );
 };
