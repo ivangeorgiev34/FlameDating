3d16011e39195ad704209fa170b9ce6516917711
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-12-23 14:47:56 +0200

    Add popupWrapper and popupCard elements

--- src/components/MatchPopup/MatchPopup.tsx
+++ src/components/MatchPopup/MatchPopup.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import styles from "./MatchPopup.module.scss";
 
 export const MatchPopup: React.FC = () => {
-  return <div>d</div>;
+  return (
+    <div className={styles.popupWrapper}>
+      <div className={styles.popupCard}></div>
+    </div>
+  );
 };
