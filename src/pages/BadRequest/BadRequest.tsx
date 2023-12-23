135ba5e773f8d4b56d6f95ff752292be5bb0c042
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-12-23 12:57:42 +0200

    Add BadRequest page component content

--- src/pages/BadRequest/BadRequest.tsx
+++ src/pages/BadRequest/BadRequest.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import styles from "./BadRequest.module.scss";
+import { Link } from "react-router-dom";
 
 export const BadRequest: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className={styles.errorWrapper}>
+      <div className={styles.errorCard}>
+        <h2 className={styles.errorTitle}>400 Bad Request</h2>
+        <p className={styles.errorDescription}>Cannot proccess your request</p>
+        <Link to={"/"}>Back to home</Link>
+      </div>
+    </div>
+  );
 };
