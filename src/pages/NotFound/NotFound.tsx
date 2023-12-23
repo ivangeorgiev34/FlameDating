1e8c78faead3f533e4b0ea1dc52099182b672102
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-12-23 12:45:10 +0200

    Add NotFound page component content

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import styles from "./NotFound.module.scss";
+import { Link } from "react-router-dom";
 
 export const NotFound: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className={styles.errorWrapper}>
+      <div className={styles.errorCard}>
+        <h2 className={styles.errorTitle}>404 Not Found</h2>
+        <p className={styles.errorDescription}>The resource cannot be found</p>
+        <Link to={"/"}>Back to home</Link>
+      </div>
+    </div>
+  );
 };
