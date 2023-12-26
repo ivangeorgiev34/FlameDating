e23bf7596a795d192149c40dcab173f41ee21174
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-12-26 13:13:39 +0200

    Add dislikeUser function

--- src/services/likeService/likeService.tsx
+++ src/services/likeService/likeService.tsx
@@ -21,3 +21,24 @@ export const likeUser = async (
     return { status: "Error", message: "Unexpected error" };
   }
 };
+
+export const dislikeUser = async (
+  token: string,
+  dislikedUserId: string
+): Promise<IResponse> => {
+  try {
+    const response = await fetch(`${BASE_URL}/like/dislike/${dislikedUserId}`, {
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      method: "POST",
+    });
+
+    const responseJson: IResponse = await response.json();
+
+    return responseJson;
+  } catch (error) {
+    return { status: "Error", message: "Unexpected error" };
+  }
+};
