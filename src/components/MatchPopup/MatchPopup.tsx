f60171d3b267a4ebdabb3062dabed9077d587bf9
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-12-27 16:46:30 +0200

    Add signalr connection and onSendMessageFormSubmit handler function

--- src/components/MatchPopup/MatchPopup.tsx
+++ src/components/MatchPopup/MatchPopup.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import styles from "./MatchPopup.module.scss";
 import IMatchPopupStateMatchedUser from "../../interfaces/matchPopup/IMatchPopupStateMatchedUser";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSpring, animated } from "@react-spring/web";
-import { useAppDispatch } from "../../hooks/reduxHooks";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import { toggleMatchPopupOff } from "../../store/matchPopup";
 import { messageValidation } from "../../validators/matchPopup/messageValidation";
 import { useError } from "../../hooks/useError/useError";
@@ -11,10 +11,26 @@ import IMatchPopupFormErrors from "../../interfaces/matchPopup/IMatchPopupFormEr
 import { formErrorsValidation } from "../../validators/formErrorsValidation/formErrorsValidation";
 import { useForm } from "../../hooks/useForm/useForm";
 import IMatchPopupForm from "../../interfaces/matchPopup/IMatchPopupForm";
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
+import { BASE_URL } from "../../constants/constants";
+import { getChatId } from "../../services/chatService/chatService";
+import { toggleLoaderOff, toggleLoaderOn } from "../../store/loader";
 
 export const MatchPopup: React.FC<IMatchPopupStateMatchedUser> = (
   props: IMatchPopupStateMatchedUser
 ) => {
+  const { token } = useAppSelector((selector) => selector.auth);
+
+  const [chatId, setChatId] = useState<string>("");
+
+  const [connection] = useState<HubConnection>(
+    new HubConnectionBuilder()
+      .withUrl(`https://localhost:7183/chatHub`, {
+        accessTokenFactory: () => (token !== null ? token : ""),
+      })
+      .build()
+  );
+
   const { formValues, onFormChange } = useForm<IMatchPopupForm>({
     message: "",
   });
@@ -23,6 +39,8 @@ export const MatchPopup: React.FC<IMatchPopupStateMatchedUser> = (
     message: "",
   });
   const dispatch = useAppDispatch();
+
+  const navigate = useNavigate();
 
   const [cardProps] = useSpring(() => ({
     from: {
@@ -36,6 +54,49 @@ export const MatchPopup: React.FC<IMatchPopupStateMatchedUser> = (
     },
   }));
 
+  const onSendMessageFormSubmit: React.FormEventHandler<
+    HTMLFormElement
+  > = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    await connection.invoke(
+      "SendMessageAsync",
+      formValues.message,
+      chatId,
+      props.id
+    );
+  };
+
+  useEffect(() => {
+    dispatch(toggleLoaderOn());
+
+    getChatId(token!, props.id)
+      .then((response) => {
+        if (response.status === "Error") {
+          if (response.message === "Not authorized") {
+            navigate("/unauthorized");
+          } else if (response.message === "User not found") {
+            navigate("/not-found");
+          }
+        } else if (response.status === "Success") {
+          setChatId(response.content?.id);
+        }
+      })
+      .finally(() => {
+        dispatch(toggleLoaderOff());
+      });
+
+    connection.start().catch(() => {
+      navigate("/internal-server-error");
+    });
+
+    return () => {
+      connection.stop().catch(() => {
+        navigate("/internal-server-error");
+      });
+    };
+  }, [connection, chatId]);
+
   return (
     <div className={styles.popupWrapper}>
       <animated.div
@@ -48,7 +109,10 @@ export const MatchPopup: React.FC<IMatchPopupStateMatchedUser> = (
         <p className={styles.matchMessage}>IT'S A MATCH</p>
         <div className={styles.sendMessageContainer}>
           <span>Be first to text {props.firstName}!</span>
-          <form className={styles.sendMessageInputContainer}>
+          <form
+            className={styles.sendMessageInputContainer}
+            onSubmit={(e) => onSendMessageFormSubmit(e)}
+          >
             <div className={styles.sendMessageInputWrapper}>
               <i className="fa-regular fa-comment-dots"></i>
               <input
