02629fbc16a3404ec7b298103018bda97e0571fd
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-02 17:38:09 +0200

    Change IRegisterFormErrors interface structure

--- src/interfaces/register/IRegisterFormErrors.tsx
+++ src/interfaces/register/IRegisterFormErrors.tsx
@@ -1,3 +1,25 @@
 import IRegisterForm from "./IRegisterForm";
 
-export default interface IRegisterFormErrors extends IRegisterForm {}
+export default interface IRegisterFormErrors {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  age: string;
+  gender: string;
+  height: string;
+  school: string;
+  job: string;
+  biography: string;
+  firstProfilePicture: string;
+  secondProfilePicture: string;
+  thirdProfilePicture: string;
+  fourthProfilePicture: string;
+  fifthProfilePicture: string;
+  maximumDistance: string;
+  preferedGender: string;
+  email: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+  location: string;
+}
