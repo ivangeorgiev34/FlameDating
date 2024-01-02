0d4f512330e984da55456a777debb3d1d2ba97e0
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-02 19:08:28 +0200

    Add type check of height variable in heightValidation

--- src/validators/register/heightValidation/heightValidation.tsx
+++ src/validators/register/heightValidation/heightValidation.tsx
@@ -1,3 +1,9 @@
-export const heightValidation = (height: number): string => {
-  return height === 0 ? "Height can't be zero" : "";
+export const heightValidation = (height: string | number): string => {
+  if (typeof height === "string") {
+    return Number.parseFloat(height.toString()) <= 0
+      ? "Height must be more than zero"
+      : "";
+  }
+
+  return height <= 0 ? "Height must be more than zero" : "";
 };
