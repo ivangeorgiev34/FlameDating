9652eb8e62352a94857af31556adeae1b9aa3d67
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-03 12:34:16 +0200

    Add check if height is empty string

--- src/validators/register/heightValidation/heightValidation.tsx
+++ src/validators/register/heightValidation/heightValidation.tsx
@@ -1,4 +1,8 @@
 export const heightValidation = (height: string | number): string => {
+  if (height === "") {
+    return "Height can't be empty";
+  }
+
   if (typeof height === "string") {
     return Number.parseFloat(height.toString()) <= 0
       ? "Height must be more than zero"
