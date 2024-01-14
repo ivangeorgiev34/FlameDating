6831981168771bff6ab6c041361ca5389c57cced
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-14 18:17:43 +0200

    Add chats state in Chats page component

--- src/pages/Chats/Chats.tsx
+++ src/pages/Chats/Chats.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Chats.module.scss";
+import IChat from "../../interfaces/chats/IChat";
 
 export const Chats: React.FC = () => {
+  const [chats, setChats] = useState<IChat[]>([]);
+
   return <div>chats</div>;
 };
