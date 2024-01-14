37e5bf719d4461a08e5332fdb19c576e079a734a
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-14 20:09:05 +0200

    Add useEffect hook for retrival of chats upon component mount

--- src/pages/Chats/Chats.tsx
+++ src/pages/Chats/Chats.tsx
@@ -1,9 +1,37 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Chats.module.scss";
 import IChat from "../../interfaces/chats/IChat";
+import { getUsersChats } from "../../services/chatService/chatService";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
+import { useNavigate } from "react-router-dom";
+import { toggleLoaderOff, toggleLoaderOn } from "../../store/loader";
 
 export const Chats: React.FC = () => {
   const [chats, setChats] = useState<IChat[]>([]);
+
+  const { token } = useAppSelector((selector) => selector.auth);
+  const dispatch = useAppDispatch();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(toggleLoaderOn());
+
+    getUsersChats(token!)
+      .then((response) => {
+        if (response.status === "Error") {
+          navigate("/not-found");
+        } else if (response.status === "Success") {
+          setChats((state) => [...state, response.content.chats]);
+        }
+      })
+      .catch(() => {
+        navigate("/internal-server-error");
+      })
+      .finally(() => {
+        dispatch(toggleLoaderOff());
+      });
+  }, [chats]);
 
   return <div>chats</div>;
 };
